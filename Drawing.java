c4478e7e3c3a3eb7aee98a125a38a014dc2adcef
Emi <emi02cd@gmail.com>
2017-01-06 22:09:47 +0900

    ウィンドウが表示される

---
+++ Drawing.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Drawing {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(200,200,800,600);
+		frame.setVisible(true);
+	}
+}
